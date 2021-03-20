3c5fb2cdb667f5bc631a8109c5b7be9829fc366c
Guillermo Barrasa <guillermo.barrasa@enti.cat>
2021-03-20 23:22:46 +0100

    Comentando código

--- pub/spaceships.js
+++ pub/spaceships.js
@@ -5,6 +5,8 @@ let that;
 
 let player_init_x = 64;
 
+//Dos background para intercambiarlos
+//y dar la sensación de movimiento
 let bg1;
 let bg2;
 
@@ -19,38 +21,40 @@ let space_key;
 let score;
 let points;
 
+//Array de meteoritos, parecida a la de coches
+//Cada uno tiene:
+//name para referirse a cada uno
+//img ruta de la imagen
+//sizeX e Y para cambar el tamaño de la hitbox de las fisicas más adelante
 let meteor_list = [
 	{
 		name: "meteorDL",
 		img: "meteor_detailedLarge.png",
 		sizeX: 36,
 		sizeY: 36,
-		speed: 1
 	},
 	{
 		name: "meteorSDL",
 		img: "meteor_squareDetailedLarge.png",
 		sizeX: 36,
 		sizeY: 36,
-		speed: 1
 	},
 	{
 		name: "meteorDS",
 		img: "meteor_detailedSmall.png",
 		sizeX: 24,
 		sizeY: 24,
-		speed: 1
 	},
 	{
 		name: "meteorSDS",
 		img: "meteor_squareDetailedSmall.png",
 		sizeX: 24,
 		sizeY: 24,
-		speed: 1
 	}
 ];
 
-let before_up;
+//Variable para hacer que solo se dispare una bala por pulsación
+let before_up; 
 
 let particle;
 
@@ -64,6 +68,7 @@ const SCREEN_MARGIN = 32;
 
 function preload () {
 	
+	//Esto lo usaremos para los problemas con scope más adelante
 	that = this;
 
 	this.load.image("background", "stars.jpg");
@@ -89,9 +94,11 @@ function create () {
 	bg2.setScale(1.25);
 
 	player = this.physics.add.image(player_init_x, scene_h/2, "ship");
+	//Cambiamos el tamaño de la hitbox de la nave
 	player.setSize(42, 32);
 	player.setOffset(8, 16);
 	player.setScale(1);
+	//La rotamos para que mire hacia la derecha
 	player.angle = 90;
 
 
@@ -99,8 +106,11 @@ function create () {
 	for (let i = 0; i < MAX_ENEMIES; i++){
 		let x = Math.random()*scene_w*10 + scene_w/2;
 		let y = Math.random()*scene_h;
+		//Este random decide qué tipo de meteorito crea
 		let rand_type = Math.floor(Math.random()*4);
 		enemies.push(this.physics.add.image(x, y, meteor_list[rand_type].name));
+		//Cambiamos el tamaño de la hitbox de cada meteorito
+		//según lo que hemos decidido antes
 		enemies[i].setSize(meteor_list[rand_type].sizeX, meteor_list[rand_type].sizeY);	
 
 	}
@@ -117,11 +127,14 @@ function create () {
 	down_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 	space_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
+	//Colisión de meteorito con player
 	enemies.forEach(function(element){
 		that.physics.add.overlap(player, element, function (p, m){
 			that.scene.restart();
 		}, null, that);
 	});
+
+	//Colisión de meteorito con bala
 	enemies.forEach(function(element){
 		that.physics.add.overlap(bullets, element, function (b, m){
 			m.destroy();
@@ -178,7 +191,10 @@ function update () {
 			}
 		}
 	}
-
+	
+	//Este simple if hace posible que solo se dispare una
+	//sola bala por cada vez que se pulsa al espacio
+	//(Mirar la condición if de arriba)
 	if(space_key.isUp){
 		before_up = true;
 	}
